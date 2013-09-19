7f2f4822a77d6fa3afae5672a535dd41de7cd73d
jonnakarlsson <jonna@tassen.se>
2013-09-19 23:35:10 +0200

    New methods in Stat controller and new gsp-pages to show results of methods.

--- grails-app/controllers/survey/StatisticsController.groovy
+++ grails-app/controllers/survey/StatisticsController.groovy
@@ -2,11 +2,12 @@ package survey
 import java.text.SimpleDateFormat
 class StatisticsController {
 
+	SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
+	def allQuestions = SurveyQuestion.list()
+
 	def index(){
 		if (request.post){
 
-			SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
-			def allQuestions = SurveyQuestion.list()
 			Date sd = dateFormat.parse(params.startDate)
 			Date ed = dateFormat.parse(params.endDate)
 			def listQACs = []
@@ -23,9 +24,68 @@ class StatisticsController {
 						totValue = totValue + Integer.parseInt(a.answerValue)
 					}
 				}
-                listQACs << [question: questionText, avrage: countAnswers ? totValue/countAnswers : 0, totAnswers: countAnswers]
+				listQACs << [question: questionText, avrage: countAnswers ? totValue/countAnswers : 0, totAnswers: countAnswers]
 			}
-            return [listWithQuestionAvrageAndCount: listQACs]
+			return [listWithQuestionAvrageAndCount: listQACs]
+		}
+	}
+	def onlyGrade(){
+		if (request.post){
+			
+			Date sd = dateFormat.parse(params.startDate)
+			Date ed = dateFormat.parse(params.endDate)
+			def allGradeQuestions = allQuestions.findAll{ SurveyQuestion ->
+				SurveyQuestion.getQuestionType() == 1
+			}
+			def listQACs = []
+
+			for (q in allGradeQuestions){
+				def questionText = q.questionText;
+				int countAnswers = 0;
+				int totValue = 0;
+				def allGradeAnswers = SurveyAnswer.findAllByQuestionAndAnswerDateBetween(q, sd, ed)
+
+				for (a in allGradeAnswers){
+
+					countAnswers ++
+					totValue = totValue + Integer.parseInt(a.answerValue)
+				}
+				listQACs << [question: questionText, avrage: countAnswers ? totValue/countAnswers : 0, totAnswers: countAnswers]
+			}
+			return [listWithQuestionAvrageAndCount: listQACs]
+		}
+	}
+	def onlyBoolean(){
+		if (request.post){
+			
+			Date sd = dateFormat.parse(params.startDate)
+			Date ed = dateFormat.parse(params.endDate)
+			def allBooleanQueestions = allQuestions.findAll{ SurveyQuestion ->
+				SurveyQuestion.getQuestionType == 2
+			}
+			def listQCP = []
+
+			for (q in allBooleanQuestions){
+				def questionText = q.questionText;
+				int countAnswers = totTrue + totFalse; //flytta ner i mappen
+				int totTrue = 0;
+				int totFalse = 0;
+				def allBooleanAnswers = SurveyAnswer.findAllByQuestionAndAnswerDateBetween(q, sd, ed)
+
+				for (a in allBooleanAnswers){
+					if (Boolean.parseBoolean(a.answerValue) == true){
+						totTrue ++
+					}
+					else if (Boolean.parseBoolean(a.answerValue)== false){
+						totFalse ++
+					}
+
+					int trues = totTrue / countAnswers * 100;
+					int falses = totFalse / countAnswers * 100;
+					listQCP << [question: questionText, countAnswers: countAnswers, trues: trues, falses: falses ]
+				}
+				return [listWithQuestionCountAndProcentage: listQCP]
+			}
 		}
 	}
 }
---
+++ grails-app/views/statistics/type1.gsp
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<title>Type1</title>
+<meta charset="utf-8">
+<link rel="stylesheet" href="${resource(dir: 'css', file:'survey.css')}">
+</head>
+
+<body>
+	<div class="top">
+		<nav id="ToppKnappar">
+			<ul>
+				<li><a href="http://localhost:8080/survey/survey/index">
+						SVARSENKÄT </a></li>
+				<li><a href="http://localhost:8080/survey/admin/index">
+						ADMINSIDA </a></li>
+				<li><a href="http://localhost:8080/survey/statistics/index">
+						STATISTIK </a></li>
+			</ul>
+		</nav>
+		<br>
+		<h1>
+			<img src="${resource(dir: 'images', file:'djnLogo.jpg')}"
+				alt="Logo Djuronaset">
+		</h1>
+	</div>
+
+<div class="body">
+<p><h2>Statistiksidan - Vad vill du veta?</h2></p>
+
+		<p>
+		<g:form action="onlyGrade">
+			<label>Från och med:</label> <input type="text" value="${formatDate(format: 'yyyyMMdd', date: new Date() - 30)}"
+				name="startDate" id="startDate" placeholder="ÅÅÅÅMMDD" maxlength="8"
+				autofocus="" required="">
+			<label>Till och med:</label> <input type="text" value="${formatDate(format: 'yyyyMMdd', date: new Date() + 1)}"
+				name="endDate" id="endDate" placeholder="ÅÅÅÅMMDD" maxlength="8"
+				required="">
+
+			<label>Vilka frågor vill du se? </label><select name="whichQuestions">
+							<option value="1">BETYGSFRÅGOR</option>
+							<option value="2">JA/NEJ FRÅGOR</option>
+							<option value="3">TEXTMEDDELANDEN</option>
+							<option value="4">BETYG & JA/NEJ</option>
+							<option value="5">ALLA INKL TEXT</option>
+					</select>
+			<input type="submit" value="VISA">
+			</g:form>
+		</p>
+
+		<p>
+		<table>
+			<caption>Statistik för vald tidpunkt</caption>
+			<thead>
+			<tr>
+				<th>Fråga</th>
+				<th>Antal</th>
+				<th>Medelbetyg</th>
+				</tr></thead>
+
+			<tbody>
+				<g:each in="${listWithQuestionAvrageAndCount}" var="map">
+					<tr>
+						<td>
+							${map.question}
+						</td>
+						<td>
+							${map.totAnswers}
+						</td>
+						<td>
+							${map.avrage}
+						</td>
+					</tr>
+				</g:each>
+			</tbody>
+		</table>
+		</p>
+
+	</div>
+	</body>
+
+</html>
